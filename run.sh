8630e1fb27d9bc31cf307d3b7406e46e6ccd92bd
carlos-a-g-h <carloselputohabanero@proton.me>
2022-10-24 23:30:01 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "$THE_PASSWORD"
+
 wget "https://gist.github.com/carlos-a-g-h/604075b1db1f6d53d1bedf2d29e0889a/raw/dummy_webserver.py"
 openssl enc -aes-256-cbc -d -in script -out "main.py" -k "$THE_PASSWORD"
 python3 -m dummy_webserver.py 10000
